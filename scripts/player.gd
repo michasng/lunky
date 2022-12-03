23c15449f1a7d26f798a22ca5e16cc6aa56627f8
Micha Sengotta <micha.sengotta@gmail.com>
2022-12-03 13:22:00 +0100

    Add ledge grabbing

--- scripts/player.gd
+++ scripts/player.gd
@@ -13,11 +13,15 @@ var max_speed: float = 0.0725 * spelunky2_fps * pixel_per_meter
 var sprint_factor: float = 2
 var jump_power: float = 0.18 * pow(spelunky2_fps, 2) * pixel_per_meter
 
+# how many pixels the player hangs above ledges
+var ledge_offset: float = 2
+
 @onready var tile_above: RayCast2D = $TileAbove
 @onready var tile_above_default_target_pos: Vector2i = tile_above.target_position
 @onready var tile_in_front: RayCast2D = $TileInFront
 @onready var tile_in_front_default_target_pos: Vector2i = tile_in_front.target_position
 
+@onready var hit_box: Vector2 = $HitBox.shape.get_rect().size
 @onready var sprite: Sprite2D = $Sprite2D
 @onready var anim_tree: AnimationNodeStateMachinePlayback = $AnimationTree.get("parameters/playback")
 
@@ -36,7 +40,7 @@ func _ready():
 	anim_tree.travel('enter')
 
 func _physics_process(delta: float):
-	# first transition, then handle physics, for responsiveness
+	# first transition then physics for responsiveness
 	var next_state = get_transition()
 	if next_state != current_state:
 		set_state(next_state, delta)
@@ -82,6 +86,13 @@ func _enter_state(next_state: State, _previous_state: State, delta: float):
 		anim_tree.travel("jump_fall")
 	if next_state == State.LEDGE:
 		velocity.y = 0
+		# move the player directly to the ledge
+		var player_center = position - hit_box / 2
+		var tile = floor(player_center / pixel_per_meter) * pixel_per_meter
+		var direction = tile_above.target_position.normalized()
+		if direction == Vector2.RIGHT:
+			tile += Vector2(pixel_per_meter, 0)
+		position = tile + Vector2(-direction.x * hit_box.x / 2, hit_box.y - ledge_offset)
 		anim_tree.travel("ledge_grab")
 
 
@@ -94,17 +105,18 @@ func handle_physics(delta: float):
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
-	var direction = Input.get_axis("move_left", "move_right")
-
 	velocity.x = move_toward(velocity.x, 0, friction)
+
+	var direction = Input.get_axis("move_left", "move_right")
 	if direction:
-		var factor = 1.0 if Input.is_action_pressed("sprint") else sprint_factor
+		var factor = 1.0 if Input.is_action_pressed("sneak") else sprint_factor
 		velocity.x = move_toward(
 			velocity.x,
 			direction * max_speed * factor,
 			acceleration * delta * factor
 		)
 
+	# print(position / pixel_per_meter)
 	# if velocity != Vector2.ZERO:
 	# 	print('vel: ', velocity / spelunky2_fps / pixel_per_meter)
 	move_and_slide()
@@ -115,13 +127,16 @@ func handle_physics(delta: float):
 		tile_in_front.target_position = direction * tile_in_front_default_target_pos
 	if current_state == State.GROUND:
 		if direction:
-			anim_tree.travel("walk")
+			if Input.is_action_pressed("sneak"):
+				anim_tree.travel("sneak")
+			else:
+				anim_tree.travel("walk")
 		else:
 			anim_tree.travel("idle")
 
 
 func set_state(next_state: State, delta: float):
-	print(state_names[next_state])
+	# print(state_names[next_state])
 	previous_state = current_state
 	current_state = next_state
 	_exit_state(previous_state, next_state, delta)
