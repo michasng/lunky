4d5acc3480061472a3b55a18cd877619d2611d93
Micha Sengotta <micha.sengotta@gmail.com>
2022-12-10 15:28:04 +0100

    Add hanging onto body ledges

--- scenes/player/character_states/ledge_state.gd
+++ scenes/player/character_states/ledge_state.gd
@@ -18,15 +18,30 @@ func get_transition() -> CharacterState:
 
 func enter_state(_previous_state: CharacterState, _delta: float):
 	body.velocity.y = 0
-	var tile = body.pos_to_tile(body.get_center())
-	if body.view_dir == body.RIGHT: tile.x += 1
-	var ledge_pos = body.tile_to_pos(tile, false)
-	# move the player directly to the ledge
+	var collider = body.tile_in_front.get_collider()
+	var ledge_pos: Vector2
+	if collider is CollisionObject2D:
+		var owner_ids = collider.get_shape_owners()
+		var shape_id = body.tile_in_front.get_collider_shape()
+		var shape = collider.shape_owner_get_shape(owner_ids[0], shape_id)
+		ledge_pos = collider.position + shape.get_rect().position
+		if body.view_dir == body.LEFT:
+			ledge_pos += Vector2(shape.get_rect().size.x, 0)
+	if collider is TileMap:
+		var tile = body.pos_to_tile(body.get_center())
+		if body.view_dir == body.RIGHT: tile.x += 1
+		ledge_pos = body.tile_to_pos(tile, false)
+	move_to_ledge(ledge_pos)
+
+	anim_playback.travel("ledge_grab")
+
+
+func move_to_ledge(ledge_pos: Vector2):
 	body.position = ledge_pos + Vector2(
 		-body.view_dir * (body.hit_box.x / 2 - 1), # -1 pixel, to avoid weird edge collisions
 		body.hit_box.y - ledge_offset
 	)
-	anim_playback.travel("ledge_grab")
+
 
 func exit_state(_next_state: CharacterState, _delta: float):
 	pass
